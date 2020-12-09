ca89a74b60d30821493e908dce3c950fc09871ee
league3236 <league3236@gmail.com>
2020-12-10 00:02:17 +0900

    201210

---
+++ src/SwitchCase01.java
@@ -0,0 +1,25 @@
+public class SwitchCase01 {
+    public static void main(String[] args) {
+        int var = 3;
+
+        // 짝수 홀수 여부를 판단
+        switch(var) {
+            case 1:
+                System.out.println("this is 1");
+                break;
+            case 2:
+                System.out.println("this is 2");
+                break;
+            case 3:
+                System.out.println("this is 3");
+                break;
+            case 4:
+                System.out.println("this is 4");
+                break;
+            default:
+                System.out.println("this is default");
+                break;
+        }
+
+    }
+}
