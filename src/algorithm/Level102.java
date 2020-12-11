e20e44eb0cd1cd735739e8afc10a95b657a3add2
league3236 <league3236@gmail.com>
2020-12-11 23:23:10 +0900

    201211

--- src/algorithm/Level102.java
+++ src/algorithm/Level102.java
@@ -1,21 +1,28 @@
 package algorithm;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Level102 {
     public static void main(String[] args) {
         Level102 sol = new Level102();
-        System.out.println(sol.solution(new int[] {2,1, 3, 4, 1}));
+        System.out.println(Arrays.toString(sol.solution(new int[] {2,1, 3, 4, 1})));
     }
     public int[] solution(int[] numbers) {
         Set<Integer> set = new HashSet<>();
-        int[] answer = numbers;
         for(int i = 0; i<numbers.length; i++) {
             for (int j = i+1; j <numbers.length ; j++) {
                 set.add(numbers[i] + numbers[j]);
             }
         }
-        return answer;
+
+        int[] answers = new int[numbers.length+1];
+        int k=0;
+        for (int i:
+             set) {
+            answers[k++] = i;
+        }
+        return answers;
     }
 }
--- src/statement/Stream01.java
+++ src/statement/Stream01.java
@@ -1,4 +1,25 @@
 package statement;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Stream01 {
+    public static void main(String[] args) {
+//        String[] arr = new String[]{"a", "b", "c"};
+//
+//        Stream<String> stream = Arrays.stream(arr);
+//        Stream<String> streamOfArrayPart = Arrays.stream(arr, 1, 3)
+
+        // set to array
+
+        Set<Integer> set = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+        Integer[] arrays = new Integer[set.size()];
+
+        int k = 0;
+        for (Integer i:
+             set) {
+            arrays[k++] = i;
+        }
+    }
 }
